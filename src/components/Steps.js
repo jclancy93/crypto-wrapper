9f3776ab231e2c8d1fd5adbaf2e3c84b950beff7
Jack Clancy <jack.clancy93@gmail.com>
2021-02-18 13:45:33 -0500

    Fixing package for deploy

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -42,7 +42,7 @@ const Step = ({number, name, icon, active, completed, lastStep}) => {
       {lastStep && (
       <div className="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
         <svg className="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-          <path d="M0 -2L20 40L0 82" vectorEffect="non-scaling-stroke" stroke="currentcolor" stroke-linejoin="round" />
+          <path d="M0 -2L20 40L0 82" vectorEffect="non-scaling-stroke" stroke="currentcolor" strokeLinejoin="round" />
         </svg>
       </div>
       )}
